052911168c39bc0414bfdaa3aec7dbd6a03a7ef0
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 21:44:06 +0300

    add groups and toString

--- src/main/java/com/divashchenko/model/Student.java
+++ src/main/java/com/divashchenko/model/Student.java
@@ -1,8 +1,14 @@
 package com.divashchenko.model;
 
+import javafx.beans.binding.StringBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     private String name;
     private int age;
+    private List<Group> groups = new ArrayList<>();
 
     public Student(String name, int age) {
         this.name = name;
@@ -25,11 +31,32 @@ public class Student {
         this.age = age;
     }
 
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
     @Override
     public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < groups.size(); i++) {
+            sb.append(groups.get(i).getName()).append(", ");
+            if (i == groups.size() - 1) {
+                sb.delete(sb.length() - 2, sb.length());
+            }
+        }
+
+        if (sb.length() < 1) {
+            sb.append("[]");
+        }
+
         return "Student{" +
                 "name='" + name + '\'' +
                 ", age=" + age +
+                ", groups=" + sb.toString() +
                 '}';
     }
 }
