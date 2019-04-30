2335b23ba749401c594eafaf2dba4dbc8debe63b
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 21:15:26 +0300

    add Group class and init

---
+++ src/main/java/com/divashchenko/model/Group.java
@@ -0,0 +1,25 @@
+package com.divashchenko.model;
+
+public class Group {
+
+    private String name;
+
+    public Group(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
