f5a15836616e23f951e0e0186f9c1b98d10b6015
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 21:44:21 +0300

    add students to group

--- src/main/java/com/divashchenko/model/Group.java
+++ src/main/java/com/divashchenko/model/Group.java
@@ -1,8 +1,12 @@
 package com.divashchenko.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
 
     private String name;
+    private List<Student> students = new ArrayList<>();
 
     public Group(String name) {
         this.name = name;
@@ -16,10 +20,19 @@ public class Group {
         this.name = name;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     @Override
     public String toString() {
         return "Group{" +
                 "name='" + name + '\'' +
+                ", students=" + students +
                 '}';
     }
 }
